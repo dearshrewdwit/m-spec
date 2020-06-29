676dfd06996d6c5b09f3bc70214153fd91d43022
Edward Withers <dearshrewdwit@gmail.com>
2020-06-29 16:01:21 +0100

    add output matcher

--- spec/core/feature_specs.rb
+++ spec/core/feature_specs.rb
@@ -1,12 +1,19 @@
+require 'stringio'
+
 require_relative "../../lib/m-spec/core/expect"
 require_relative "../../lib/m-spec/core/spec_error"
 require_relative "../../lib/m-spec/core/spec_result"
 require_relative "../../lib/m-spec/core/matchers/equal"
+require_relative "../../lib/m-spec/core/matchers/output"
 require_relative "../../lib/m-spec/core/helpers/readable"
 
 class Animal
   def roar
     "ROAAAARRRR!"
+  end
+
+  def show_it!
+    puts roar
   end
 end
 
@@ -19,5 +26,17 @@ describe 'The Animal' do
   it 'fails nicely' do
     animal = Animal.new
     expect(animal.roar).to eq "little roar!"
+  end
+end
+
+describe 'Testing output using expect block syntax' do
+  it 'returns the strings when it fails' do
+    expect { puts('hello') }.to output("helslo\n")
+  end
+
+  it 'successfully mocks STDOUT' do
+    animal = Animal.new
+
+    expect { animal.show_it! }.to output("ROAAAARRRR!\n")
   end
 end
